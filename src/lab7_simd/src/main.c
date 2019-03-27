549d75cc6d7b6be7337962a9a067a6ec595a8053
Carlos Requena López <crequena@ulb.ac.be>
2019-03-27 11:56:44 +0100

    Add no_simd flags to sisd compilation

--- src/lab7_simd/src/main.c
+++ src/lab7_simd/src/main.c
@@ -4,36 +4,37 @@
 #include <ctype.h>
 
 int main(int argc, char* argv[]) {
-	if (argc > 4 || argc < 4) {
-		printf("Usage: ./simd <filename> <output> <treshold>\n");
-	}
+    if (argc > 4 || argc < 4) {
+        printf("Usage: ./sisd <filename> <output> <treshold>\n");
+        return 1;
+    }
 
-	char* input_filename = argv[1];
-	char* output_filename = argv[2];
-	int treshold = atoi(argv[3]);
+    char* input_filename = argv[1];
+    char* output_filename = argv[2];
+    int treshold = atoi(argv[3]);
 
-	FILE* input = fopen(input_filename, "r");
-	FILE* output = fopen(output_filename, "w");
+    FILE* input = fopen(input_filename, "r");
+    FILE* output = fopen(output_filename, "w");
 
-	char buffer[255] = {0};
+    char buffer[256] = {0};
 
-	clock_t tstart, tend;
-	tstart = clock();
+    clock_t tstart, tend;
+    tstart = clock();
 
-	while (fgets(buffer, 256, input) != NULL) {
-		for (int i = 0; i < 255; ++i) {
-			char curr = buffer[i];
-			if ((unsigned char) curr < treshold) {
-				curr = 0;
-			} else {
-				curr = 255;
-			}
-			fwrite(&curr, sizeof(curr), 1, output);
-		}
-	}
+    while (fgets(buffer, 256, input) != NULL) {
+        for (int i = 0; i < 255; ++i) {
+            unsigned char curr = (unsigned char) buffer[i];
+            if ((unsigned char) curr < treshold) {
+                curr = 0;
+            } else {
+                curr = 255;
+            }
+            fwrite(&curr, sizeof(curr), 1, output);
+        }
+    }
 
-	tend = clock();
-	float time = (float)(tend - tstart) / CLOCKS_PER_SEC;
-	printf("Time spent: %f\n", time);
-	return EXIT_SUCCESS;
+    tend = clock();
+    float time = (float)(tend - tstart) / CLOCKS_PER_SEC;
+    printf("Time spent: %f\n", time);
+    return EXIT_SUCCESS;
 }
