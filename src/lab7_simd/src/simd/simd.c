c4f46279a49a5daff1df4ae9b8e3b5d4eb13ccf2
nik1168 <ngeisser32@gmail.com>
2019-04-03 02:10:28 +0200

    Comment code

--- src/lab7_simd/src/simd/simd.c
+++ src/lab7_simd/src/simd/simd.c
@@ -5,25 +5,31 @@
 
 #define SIM_BYTES 16
 
+
 int main(int argc, char* argv[]) {
     if (argc > 4 || argc < 4) {
         printf("Usage: ./sisd <filename> <output> <treshold>\n");
         return 1;
     }
 
+    // Receive arguments from command line
     char* input_filename = argv[1];
     char* output_filename = argv[2];
     int threshold = atoi(argv[3]);
 
+    // Get input and output varaibles
     FILE* input = fopen(input_filename, "r");
     FILE* output = fopen(output_filename, "w");
+
 
     fseek(input, 0, SEEK_END);
     long file_size = ftell(input);
     fseek(input, 0, SEEK_SET);
 
+    // Memory allocation for buffer
     unsigned char *buffer = malloc(sizeof(unsigned char) * (file_size + ((-file_size) % SIM_BYTES)));
 
+    // Initialize thresholds to act as a mask
     unsigned char thresholds[SIM_BYTES];
     for (int i = 0; i < SIM_BYTES; ++i) {
         thresholds[i] = threshold; // The data need to be put in an array to work with SIMD instructions
@@ -34,8 +40,9 @@ int main(int argc, char* argv[]) {
 
     fread(buffer, sizeof(unsigned char), file_size, input);
 
+    // Make a loop and increment counter by 16
     for (long i = 0; i < file_size; i += SIM_BYTES) {
-        unsigned char *buffer_position = (unsigned char *) (buffer + i);
+        unsigned char *buffer_position = (unsigned char *) (buffer + i); // Get the current position to transverse the image
         __asm__ __volatile__ (
             "movdqu (%2), %%xmm0;"
             "movdqu (%1), %%xmm1;"
@@ -48,6 +55,7 @@ int main(int argc, char* argv[]) {
             );
     }
 
+    // Write the buffer into the output file
     fwrite(buffer, sizeof(char), file_size, output);
 
     tend = clock();
